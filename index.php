9f7435db4a8bf567107fbd4cd1bf0bacb1837249
Tudor <pascu.tn@pg.com>
2021-02-18 19:26:48 +0200

    composer

--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php
 
-include 'factory.php';
+include 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$mail = new PHPMailer();
+
+$mail->isSMTP();
+$mail->Host = 'localhost';
+$mail->SMTPAuth = true;
+//$mail->Username = null;
+//$mail->Password = null;
+//$mail->SMTPSecure = 'tls';
+$mail->Port = 1025;
 
 
-$dacia = AutomobilFactory::create(1300);
-echo $dacia->getMake();
+$mail->setFrom('info@mailtrap.io', 'Mailtrap');
+$mail->addReplyTo('info@mailtrap.io', 'Mailtrap');
+$mail->addAddress('recipient1@mailtrap.io', 'Tim');
+$mail->addCC('cc1@example.com', 'Elena');
+$mail->addBCC('bcc1@example.com', 'Alex');
+
+$mail->Subject = 'Test Email via Mailtrap SMTP using PHPMailer';
+
+$mail->isHTML(true);
+
+$mailContent = "<h1>Send HTML Email using SMTP in PHP</h1>
+    <p>This is a test email I’m sending using SMTP mail server with PHPMailer.</p>";
+$mail->Body = $mailContent;
+
+if($mail->send()){
+    echo 'Message has been sent';
+}else{
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
